82e76af25004bc7b6830dbe0e2a86a6e6b0a12d7
YunHo Choi <svs101@dgu.ac.kr>
2022-09-05 02:36:42 +0900

    Swagger 연동

--- src/main/java/com/curady/lectureservice/LectureServiceApplication.java
+++ src/main/java/com/curady/lectureservice/LectureServiceApplication.java
@@ -1,13 +1,19 @@
 package com.curady.lectureservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@OpenAPIDefinition(info =
+    @Info(title = "Lecture API", version = "1.0", description = "Documentation Lecture API v1.0")
+)
 public class LectureServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LectureServiceApplication.class, args);
     }
-
 }

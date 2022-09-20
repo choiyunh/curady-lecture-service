6f609c211c41b039da24bdb65f0540e6d91d878f
YunHo Choi <svs101@dgu.ac.kr>
2022-09-21 02:02:37 +0900

    Feat/#7: 카테고리 조회 API 구현
    - Controller 추가
    - Response Dto 추가

---
+++ src/main/java/com/curady/lectureservice/web/lecture/controller/CategoryController.java
@@ -0,0 +1,30 @@
+package com.curady.lectureservice.web.lecture.controller;
+
+import com.curady.lectureservice.domain.category.service.CategoryService;
+import com.curady.lectureservice.global.result.MultipleResult;
+import com.curady.lectureservice.global.service.ResponseService;
+import com.curady.lectureservice.web.lecture.dto.ResponseCategory;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class CategoryController {
+    private final CategoryService categoryService;
+    private final ResponseService responseService;
+
+    @Operation(description = "parentId별 카테고리 목록을 조회합니다.")
+    @GetMapping("/categories/{parentId}")
+    public MultipleResult<ResponseCategory> getCategories(@PathVariable Long parentId) {
+        List<ResponseCategory> responseCategories = categoryService.getCategoryByParentId(parentId);
+
+        return responseService.getMultipleResult(responseCategories);
+    }
+}
---
+++ src/main/java/com/curady/lectureservice/web/lecture/dto/ResponseCategory.java
@@ -0,0 +1,12 @@
+package com.curady.lectureservice.web.lecture.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class ResponseCategory {
+    private String name;
+}

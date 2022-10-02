6f80050643e8e138e45cca4829558c556bf4bd77
YunHo Choi <svs101@dgu.ac.kr>
2022-10-03 03:02:05 +0900

    Feat/#11: 강의 조회 API
    - instructor 버그 수정

--- src/main/java/com/curady/lectureservice/mapper/LectureMapper.java
+++ src/main/java/com/curady/lectureservice/mapper/LectureMapper.java
@@ -1,10 +1,11 @@
 package com.curady.lectureservice.mapper;
 
 import com.curady.lectureservice.domain.lecture.model.Lecture;
-import com.curady.lectureservice.web.lecture.dto.ResponseLecture;
 import com.curady.lectureservice.web.lecture.dto.ResponseLectures;
+import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -13,8 +14,10 @@ import java.util.List;
 public interface LectureMapper {
     LectureMapper INSTANCE = Mappers.getMapper(LectureMapper.class);
 
+    @Named("E2R")
     @Mapping(target = "instructorName", source = "instructor.name")
-    ResponseLecture lectureToResponse(Lecture lecture);
-    @Mapping(target = "instructorName", source = "instructor.name")
+    ResponseLectures lectureToResponse(Lecture lecture);
+
+    @IterableMapping(qualifiedByName = "E2R")
     List<ResponseLectures> lecturesToResponses(List<Lecture> lecture);
 }

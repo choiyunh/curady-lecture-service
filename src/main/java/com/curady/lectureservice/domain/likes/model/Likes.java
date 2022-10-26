731fc4abd846f90320019e7b1074a0050e1d5175
YunHo Choi <svs101@dgu.ac.kr>
2022-10-27 02:39:49 +0900

    Feat: 좋아요한 강의 목록 조회(#19)

--- src/main/java/com/curady/lectureservice/domain/likes/model/Likes.java
+++ src/main/java/com/curady/lectureservice/domain/likes/model/Likes.java
@@ -2,6 +2,7 @@ package com.curady.lectureservice.domain.likes.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
@@ -11,6 +12,7 @@ import javax.persistence.Id;
 
 @Entity
 @Builder
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Likes {
--- src/main/java/com/curady/lectureservice/domain/likes/repository/LikesRepository.java
+++ src/main/java/com/curady/lectureservice/domain/likes/repository/LikesRepository.java
@@ -3,10 +3,13 @@ package com.curady.lectureservice.domain.likes.repository;
 import com.curady.lectureservice.domain.likes.model.Likes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LikesRepository extends JpaRepository<Likes, Long> {
     Optional<Likes> findByUserIdAndLectureId(Long userId, Long lectureId);
 
     void deleteByUserIdAndLectureId(Long userId, Long lectureId);
+
+    List<Likes> findAllByUserId(Long userId);
 }
